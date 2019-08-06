140bb97426e950c6428fa68796f3d09b9ee8376c
Dmitry <frontender@inbox.ru>
2019-08-06 22:57:34 +0400

    fix some bugs

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -4,17 +4,18 @@ if (isset($_POST) && count($_POST) > 0) {
     $codeWord = "nd82jaake";
 
     foreach ($_POST as $key => $value) {
+        $format_value = trim($value);
         if ($key === "login" && preg_match("/[@\/\*\?,;:]/", $value) === 1) {
             array_push($messages, "Поле 'Логин' не должно содержать символы @/*?,;:");
-        } elseif ($key === "password" && strlen($value) < 8) {
+        } elseif ($key === "password" && strlen($format_value) < 8) {
             array_push($messages, "Длина пароля должна быть минимум 8 символов");
         } elseif ($key === "email" && preg_match("/^[A-Za-z0-9_.-]+\@\w+\.\D{2,7}/", $value) !== 1) {
             array_push($messages, "Почта должна быть формата почта@домен.доменнаязона");
-        } elseif ($key === "firstName" && strlen($value) === 0) {
+        } elseif ($key === "firstName" && strlen($format_value) === 0) {
             array_push($messages, "Имя обязательно к заполнению");
-        } elseif ($key === "lastName" && strlen($value) === 0) {
+        } elseif ($key === "lastName" && strlen($format_value) === 0) {
             array_push($messages, "Фамилия обязательна к заполнению");
-        } elseif ($key === "middleName" && strlen($value) === 0) {
+        } elseif ($key === "middleName" && strlen($format_value) === 0) {
             array_push($messages, "Отчество обязательно к заполнению");
         } elseif ($key === "code" && $value !== $codeWord) {
             array_push($messages, "Кодовое слово введено неверно");
@@ -25,11 +26,8 @@ if (isset($_POST) && count($_POST) > 0) {
         foreach ($messages as $value) {
             echo $value;
             echo "<br/>";
-        } 
-?>
-        <br/>
-        <a href="/bphp-homeworks/1.2-strings-and-forms/1.2.1-registration/form.html">Вернуться к форме</a>
-<?php
+        }
+        echo '<br/><a href="./form.html">Вернуться к форме</a>';
     } else {
         echo "Спасибо! Регистрация прошла успешно!";
     }
--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -4,17 +4,21 @@ function mb_ucfirst($text)
     return mb_strtoupper(mb_substr($text, 0, 1)) . mb_substr($text, 1);
 }
 
-if (isset($_POST)) {
+if (isset($_POST) && count($_POST) > 0) {
     $firstName = mb_ucfirst(mb_strtolower(trim($_POST["firstName"])));
     $lastName = mb_ucfirst(mb_strtolower(trim($_POST["lastName"])));
     $middleName = mb_ucfirst(mb_strtolower(trim($_POST["middleName"])));
 
-    $fullName = $lastName . " " . $firstName . " " . $middleName;
-    $fio = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($middleName, 0, 1);
-    $surnameAndInitials = $lastName . " " . mb_substr($firstName, 0, 1) . "." .  mb_substr($middleName, 0, 1) . ".";
+    if (strlen($firstName) > 0 && strlen($lastName) > 0 && strlen($middleName) > 0) {
+        $fullName = $lastName . " " . $firstName . " " . $middleName;
+        $fio = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($middleName, 0, 1);
+        $surnameAndInitials = $lastName . " " . mb_substr($firstName, 0, 1) . "." .  mb_substr($middleName, 0, 1) . ".";
 
-    echo "Полное имя: '" . $fullName . "' <br/>";
-    echo "Фамилия и инициалы: '" . $surnameAndInitials . "' <br/>";
-    echo "Аббревиатура: '" . $fio . "'"; 
+        echo "Полное имя: '" . $fullName . "' <br/>";
+        echo "Фамилия и инициалы: '" . $surnameAndInitials . "' <br/>";
+        echo "Аббревиатура: '" . $fio . "'"; 
+    } else {
+        echo '<a href="./form.html">К форме</a>';
+    }
 }
 ?>
