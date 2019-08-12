1cb03535bb2d14f8e8fa9fb3fad9cbcb2d89df3e
Dmitry <frontender@inbox.ru>
2019-08-12 22:05:59 +0400

    task 3.1.1 done

--- 3.1-exceptions-and-headers/3.1.1-router/index.php
+++ 3.1-exceptions-and-headers/3.1.1-router/index.php
@@ -5,3 +5,21 @@
  * @var array $availableLinks
  */
 $availableLinks = include './availableLinks.php';
+include './classes/Router.php';
+
+if (isset($_GET['page'])) {
+    $pagename = $_GET['page'];
+    $router = new Router($availableLinks);
+
+    try {
+        if ($router->isAvailablePage($pagename)) {
+            echo 'Вы находитесь на странице ' . $pagename;
+        }
+    } catch (Exception $e) {
+        header('HTTP/1.1 404 Not Found');
+        header('Location: 404.php');
+    }
+} else {
+    header('HTTP/1.1 400 Bad Request');
+    header('Location: error.php');
+}
